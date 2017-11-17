c3b61071dd9523751e3f1083fd65896b1bda75f1
vishwas <vishwas247@gmail.com>
2017-11-17 17:01:46 +0530

    user signup created

--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,21 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_new')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
